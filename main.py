59e21dd5b6c6fa4427bc1bf42043dc3e92854bc5
Diyaneshwar-P <dp7925@srmist.edu.in>
2026-02-28 14:03:25 +0530

    diamond pattern in python

---
+++ main.py
@@ -0,0 +1,6 @@
+rows=int(input("Enter number of rows: "))
+for i in range(rows):
+    print(" "*(rows-i)+" *"*i)
+print(' '*(i-2)+'{} rows'.format(rows))
+for i in range(rows):
+    print(" "*(i)+" *"*(rows-i))
